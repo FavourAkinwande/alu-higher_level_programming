e62b78e067286eaccbb95d16ae33064cd5ceda61
MyUser <f.akinwande@alustudent.com>
2023-07-31 08:03:10 -0700

    Ad Task

--- python-network_1/100-github_commits.py
+++ python-network_1/100-github_commits.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
+"""Lists the 10 most recent commits on a given GitHub repository.
+
+Usage: ./100-github_commits.py <repository name> <repository owner>
 """
-takes your Github credentials (username and password) and uses the
-Github API to display your id
-"""
+import sys
 import requests
-from sys import argv
 
 
 if __name__ == "__main__":
-    """
-    takes your Github credentials (username and password) and uses the
-    Github API to display your id
-    """
-    username = argv[1]
-    password = argv[2]
-    url = 'https://api.github.com/user'
-    r = requests.get(url, auth=(username, password))
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    r = requests.get(url)
+    commits = r.json()
     try:
-        print(r.json().get('id'))
-    except:
-        pass`
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
