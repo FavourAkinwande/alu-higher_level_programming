af23e3a07d3723e208a9a68299a5a4e113e7ba41
FavourAkinwande <f.akinwande@alustudent.com>
2023-08-13 11:47:48 -0700

    Task 7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-""" module gets all states """
+"""
+Script that lists all lists all `State`
+objects from the database `hbtn_0e_6_usa`.
+Arguments:
+    mysql username (str)
+    mysql password (str)
+    database name (str)
+"""
 
 import sys
 from sqlalchemy import (create_engine)
