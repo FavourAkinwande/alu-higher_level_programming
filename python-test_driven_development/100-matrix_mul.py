797c33449479e23f12fd2f6301621caab2ebe472
FavourAkinwande <f.akinwande@alustudent.com>
2023-08-18 08:45:19 -0700

    Ad Task

--- python-test_driven_development/100-matrix_mul.py
+++ python-test_driven_development/100-matrix_mul.py
@@ -1,59 +1,74 @@
 #!/usr/bin/python3
-# 100-matrix_mul.py
-"""Defines a matrix multiplication function."""
+"""
+doc
+"""
+
+
+def test_int(x):
+    return all(isinstance(ele, int) for ele in x)
+
+
+def test_float(x):
+    return all(isinstance(ele, float) for ele in x)
+
+
+def test_row(mtrx):
+    n = len(mtrx[0])
+    for row in mtrx:
+        if len(row) != n:
+            return False
+    return True
 
 
 def matrix_mul(m_a, m_b):
-    """Multiply two matrices.
-    Args:
-        m_a (list of lists of ints/floats): The first matrix.
-        m_b (list of lists of ints/floats): The second matrix.
-    Raises:
-        TypeError: If either m_a or m_b is not a list of lists of ints/floats.
-        TypeError: If either m_a or m_b is empty.
-        TypeError: If either m_a or m_b has different-sized rows.
-        ValueError: If m_a and m_b cannot be multiplied.
-    Returns:
-        A new matrix representing the multiplication of m_a by m_b.
     """
-    if m_a == [] or m_a == [[]]:
-        raise ValueError("m_a can't be empty")
-    if m_b == [] or m_b == [[]]:
-        raise ValueError("m_b can't be empty")
+    doc
+    """
     if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
-    if not all(isinstance(row, list) for row in m_a):
+    if not all(isinstance(x, list) for x in m_a):
         raise TypeError("m_a must be a list of lists")
-    if not all(isinstance(row, list) for row in m_b):
+    if not all(isinstance(x, list) for x in m_b):
         raise TypeError("m_b must be a list of lists")
-    if not all((isinstance(ele, int) or isinstance(ele, float))
-               for ele in [num for row in m_a for num in row]):
+    if m_a == []:
+        raise ValueError("m_a can't be empty")
+    if all(len(x) for x in m_a) == 0:
+        raise ValueError("m_a can't be empty")
+    if m_b == []:
+        raise ValueError("m_b can't be empty")
+    if all(len(x) for x in m_b) == 0:
+        raise ValueError("m_b can't be empty")
+    if not all(test_int(x) for x in m_a)\
+            and not all(test_float(x) for x in m_a):
         raise TypeError("m_a should contain only integers or floats")
-    if not all((isinstance(ele, int) or isinstance(ele, float))
-               for ele in [num for row in m_b for num in row]):
+    if not all(test_int(x) for x in m_b)\
+            and not all(test_float(x) for x in m_b):
         raise TypeError("m_b should contain only integers or floats")
-    if not all(len(row) == len(m_a[0]) for row in m_a):
-        raise TypeError("each row of m_a must should be of the same size")
-    if not all(len(row) == len(m_b[0]) for row in m_b):
-        raise TypeError("each row of m_b must should be of the same size")
+    if not test_row(m_a):
+        raise TypeError("each row of m_a must be of the same size")
+    if not test_row(m_b):
+        raise TypeError("each row of m_b must be of the same size")
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
-    inverted_b = []
-    for r in range(len(m_b[0])):
-        new_row = []
-        for c in range(len(m_b)):
-            new_row.append(m_b[c][r])
-        inverted_b.append(new_row)
-    new_matrix = []
-    for row in m_a:
-        new_row = []
-        for col in inverted_b:
-            prod = 0
-            for i in range(len(inverted_b[0])):
-                prod += row[i] * col[i]
-            new_row.append(prod)
-        new_matrix.append(new_row)
 
-    return new_matrix
+    r1 = []
+    i1 = 0
+
+    for a in m_a:
+        r2 = []
+        i2 = 0
+        num = 0
+        while (i2 < len(m_b[0])):
+            num += a[i1] * m_b[i1][i2]
+            if i1 == len(m_b) - 1:
+                i1 = 0
+                i2 += 1
+                r2.append(num)
+                num = 0
+            else:
+                i1 += 1
+        r1.append(r2)
+
+    return r1
