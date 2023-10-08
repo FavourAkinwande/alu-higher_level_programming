b880df35d539476a01a4803b5349f0decd2c911a
MyUser <f.akinwande@alustudent.com>
2023-10-08 11:03:58 -0700

    2-statuscode.js

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-
+// Writing a script that display the status code of a GET request.
 const request = require('request');
-const url = process.argv[2];
-request
-  .get(url)
-  .on('response', function (response) {
-    console.log('code: ' + response.statusCode);
-  });
+
+request.get(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
